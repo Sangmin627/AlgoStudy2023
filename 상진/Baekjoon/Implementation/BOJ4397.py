24954b33937dde33b8617c5ba3f7bf066ef54310
leesangjin <tkdwls0628@gmail.com>
2023-01-07 20:27:55 +0900

    [Failed] Initial solving

---
+++ 상진/Baekjoon/Implementation/BOJ4397.py
@@ -0,0 +1,46 @@
+n = int(input())
+
+board = []
+myInput = []
+answer = [["."] * n for _ in range(n)]
+bomb = []
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+flag = True
+for x in range(n):
+    board.append(list(input()))
+    for y in range(n):
+        if board[x][y] == "*":
+            bomb.append((x, y))
+
+for x in range(n):
+    myInput.append(list(input()))
+    for y in range(n):
+        if myInput[x][y] == "x" and (x, y) in bomb:
+            flag = False
+
+if not flag:
+    answer = board
+    for x in range(n):
+        for y in range(n):
+            print(answer[x][y], end="")
+        print()
+else:
+    for x in range(n):
+        for y in range(n):
+            if myInput[x][y] == "x":
+                cnt = 0
+                for i in range(8):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if 0 <= nx < n and 0 <= ny < n:
+                        if (nx, ny) in bomb:
+                            cnt += 1
+                answer[x][y] = cnt
+
+    for x in range(n):
+        for y in range(n):
+            print(answer[x][y], end="")
+        print()
