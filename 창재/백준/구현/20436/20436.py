340b833f87ed2650656b092b0eab062e680a2fd5
lcj1204 <dlckdwo1204@naver.com>
2023-01-10 19:56:15 +0900

    FEAT: [1차] 백준 20436번 풀이

---
+++ 창재/백준/구현/20436/20436.py
@@ -0,0 +1,45 @@
+def searchIndex(str):
+    for i in range(3):
+        for j in range(10):
+            if keyboard[i][j] == str:
+                return [i, j]
+
+def distance(last, cur):
+    dis = abs((last[0] - cur[0])) + abs((last[1] - cur[1]))
+    return dis
+
+
+if __name__ == '__main__':
+    keyboard = []
+    keyboard.append(list("qwertyuiop"))
+    keyboard.append(list("asdfghjkl "))
+    keyboard.append(list("zxcvbnm   "))
+
+    a = list(input().replace(' ', ''))
+    start_L = searchIndex(a[0])
+    start_R = searchIndex(a[1])
+
+    b = list(input())
+    ZOAC = []
+    for i in b:
+        ZOAC.append(searchIndex(i))
+
+    sum = 0
+    last_L, last_R = start_L, start_R
+    for s in ZOAC:
+        if s[0] <= 1:
+            if s[1] <= 4:
+                sum += distance(last_L, s) + 1
+                last_L = s
+            else:
+                sum += distance(last_R, s) + 1
+                last_R = s
+        else:
+            if s[1] <= 3:
+                sum += distance(last_L, s) + 1
+                last_L = s
+            else:
+                sum += distance(last_R, s) + 1
+                last_R = s
+
+    print(sum)
