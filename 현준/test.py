2f5336a2adfbd963ca4dfc7cf760ac469448268d
금현준 <qhofr3567@gmail.com>
2023-01-11 17:19:09 +0900

    그리디_백준2217

--- 현준/test.py
+++ 현준/test.py
@@ -1,24 +1,2 @@
-left = [
-	['q','w','e','r','t'],
-	['a','s','d','f','g'],
-	['z','x','c','v']
-]
-
-right = [
-	['y','u','i','o','p'],
-	['h','j','k','l'],
-	['b','n','m']
-]
-
-def distance_left(x):
-	for i in range(3):
-		if x in left[i]:
-			idx_tmp = left[i].index(x)
-			idx_first=[i,idx_tmp]
-			return idx_first
-		
-print(distance_left('w'))
-
-test = 'test'
-for i in test:
-	print(i)
+arr = [10]
+print(arr[-1])
