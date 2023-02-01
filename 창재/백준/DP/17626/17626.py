b14eec075fd4414e2af078f052aaa695a49e2d8e
lcj1204 <dlckdwo1204@naver.com>
2023-02-01 20:59:09 +0900

    시간 초과

--- 창재/백준/DP/17626/17626.py
+++ 창재/백준/DP/17626/17626.py
@@ -1,17 +1,16 @@
 import sys
 
-# input = sys.stdin.readline
-n = int(input())
+n = int(sys.stdin.readline())
 
 dp = [0, 1]
 
 for i in range(2, n + 1):
     min_value = 10
-    j = 1
+    r = 1
 
-    while j**2 <= i:
-        min_value = min(min_value, dp[i - j**2])
-        j += 1
+    while r**2 <= i:
+        min_value = min(min_value, dp[i - r**2])
+        r += 1
 
     dp.append(min_value + 1)
 
