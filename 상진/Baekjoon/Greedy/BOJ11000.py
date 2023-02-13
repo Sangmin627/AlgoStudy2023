086b87cd55c8529d72946ae9b6a35494d45168c1
leesangjin <tkdwls0628@gmail.com>
2023-02-13 18:14:11 +0900

    [Solved] BOJ11000

--- 상진/Baekjoon/Greedy/BOJ11000.py
+++ 상진/Baekjoon/Greedy/BOJ11000.py
@@ -1,27 +1,17 @@
-import sys
+import sys, heapq
 input = sys.stdin.readline
 
 n = int(input())
 times = []
 for i in range(n):
     start, end = map(int, input().split())
-    times.append((start, end, i))
-times.sort(key=lambda x: (x[0], x[1]))
+    times.append((start, end))
+times.sort(key=lambda x: x[0])
 
-visited = [0] * n
-answer = 0
+hq = [0]
+for start, end in times:
+    if start >= hq[0]:
+        heapq.heappop(hq)
+    heapq.heappush(hq, end)
 
-idx = 1
-while sum(visited) != n:
-    before_start, before_end, original_idx = times[idx - 1]
-    next_start, next_end, next_original_idx = times[idx]
-    visited[original_idx] = 1
-    if not visited[next_original_idx]:
-        if next_start >= before_end:
-            idx += 1
-        elif next_start < before_end:
-            answer += 1
-            times.append(times.pop(idx))
-        visited[next_original_idx] = 1
-
-print(answer if answer != 0 else 1)
+print(len(hq))
