868dcd66b255a633a0b453c04978491f7173a5b5
lcj1204 <dlckdwo1204@naver.com>
2023-02-17 11:57:16 +0900

    *1092 풀이 (다시 풀어볼 것!)

--- 창재/백준/그리디/week6/11000/11000.py
+++ 창재/백준/그리디/week6/11000/11000.py
@@ -1,8 +1,9 @@
 """
-- 직관적인 방법 : N x 10^9 개의 리스트를 만듦 -> 메모리 초과일거 같음
+- 직관적인 방법 : N x 10^9 개의 리스트를 만듦 -> 메모리 초과
 - 알고리즘 방법 : 최소..
 1. 입력을 모두 받음 [(S_1, T_1), (S_2, T_2), (S_3, T_3), ...]
-2. 정렬하면 시간이 빠른 수업부터 배치시킬 수 있음.
+2. 정렬하면 시간이 순서대로 맨 뒤 값만 참조하여 배치시킬 수 있음.
+3. 최악 : O(N^2)..
 """
 import sys
 import time
@@ -10,67 +11,55 @@ import time
 input = sys.stdin.readline
 N = int(input())
 
-
-start_0 = time.time()
-Classes = [0] * (N)
+Classes = [0] * N
 
 for i in range(N):
     s, t = map(int, input().split(' '))
     Classes[i] = (s, t)
     print(Classes)
 
-end_0 = time.time()
-print('0. 경과 시간 : ', (end_0 - start_0) * 100000)
-
-start_1 = time.time()
 # 정렬
 Classes.sort()
 print(Classes)
 
-end_1 = time.time()
-print('1. 경과 시간 : ', (end_1 - start_1) * 100000)
-
-
-start_2 = time.time()
-# 시간이 빠른 수업 부터 배치 시작.
-# room = []
-# for Class in Classes:
-#     if not room:    # room 초기화
-#         room.append([Class])
-#         # cnt += 1
-#         continue
-#
-#     flag = False # 추가 됐는지 않 됐는지
-#     for i in range(len(room)):
-#         if Class[0] >= room[i][-1][1]:
-#             room[i].append(Class)
-#             flag = True
-#             break
-#
-#     if not flag:
-#         room.append([Class])
 cnt = 0
-while Classes:
+while Classes:  # 리스트 가 빌때 까지
     i = 1
     init = Classes[0][1]
     cnt += 1
 
-    while i < N:
-        if Classes[i][0] >= init:
-            init = Classes[i][1]
+    while i < N:    # Classes 를 처음 부터 끝까지 돌겠다는 의미.
+        if Classes[i][0] >= init:   # 다음 수업의 시작이 앞 수업의 끝보다 같거나 크면,
+            init = Classes[i][1]    # 새로 임명. 그리고 pop
             Classes.pop(i)
             print(i, '인덱스 pop : ', Classes)
-            N -= 1
+            N -= 1  # i 는 그대로 냅두고, 총 길이(N)를 줄임. pop 하면서 인덱스가 하나씩 당겨지니까.
         else:
-            i += 1
+            i += 1  # 다음 수업의 시작이 앞 수업의 끝보다 작으면, 다음 수업으로 넘어감.
 
     Classes.pop(0)
     N -= 1
 
-end_2 = time.time()
-print('2. 경과 시간 : ', (end_2 - start_2) * 100000)
+print(cnt)
+
+# 시간이 빠른 수업 부터 배치 시작.
+room = []
+for Class in Classes:
+    if not room:    # room 초기화
+        room.append([Class])
+        # cnt += 1
+        continue
+
+    flag = False # 추가 됐는지 않 됐는지
+    for i in range(len(room)):
+        if Class[0] >= room[i][-1][1]:
+            room[i].append(Class)
+            flag = True
+            break
+
+    if not flag:
+        room.append([Class])
 
 # print(room)
 # print(len(room))
-print(cnt)
 
