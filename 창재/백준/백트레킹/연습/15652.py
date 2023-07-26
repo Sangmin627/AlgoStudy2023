cdda8637bb9cee04681673feda8ee3ecb272f660
lcj1204 <dlckdwo1204@naver.com>
2023-07-26 18:10:37 +0900

    [백준] 16938 - 백트래킹

---
+++ 창재/백준/백트레킹/연습/15652.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+answer = []
+
+def dfs():
+    if sorted(answer) != answer:
+        return
+
+    if len(answer) == M:
+        print(*answer)
+        return
+
+    for i in range(1, N + 1):
+        answer.append(i)
+        dfs()
+        answer.pop()
+
+
+dfs()
