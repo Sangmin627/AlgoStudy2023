bd426a69e6e8fe6f3ff321ecb8b30b27aa0e60dc
lcj1204 <dlckdwo1204@naver.com>
2024-05-13 18:06:55 +0900

    [백준] 4811 - DP

---
+++ 창재/백준/DP/혼자/4811.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+while 1:
+    N = int(input())
+    if N == 0:
+        break
+
+    dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+    for i in range(N + 1):
+        dp[0][i] = 1
+
+    for i in range(1, N + 1):
+        for j in range(N + 1 - i):
+            if j == 0:
+                dp[i][j] = dp[i - 1][j + 1]
+            else:
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j + 1]
+k
+    print(dp[N][0])
