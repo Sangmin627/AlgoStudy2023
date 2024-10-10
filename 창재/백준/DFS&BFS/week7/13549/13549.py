9f3f9fdd1dcb8f9e5dd3e2be8b5a4937206c1397
lcj1204 <dlckdwo1204@naver.com>
2024-10-10 20:23:22 +0900

    [백준] 13549 - BFS

--- 창재/백준/DFS&BFS/week7/13549/13549.py
+++ 창재/백준/DFS&BFS/week7/13549/13549.py
@@ -4,15 +4,34 @@ from collections import deque
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
+LIMIT = 100001
+if K <= N:
+    print(N - K)
+else:
+    q = deque()
+    q.append(N)
 
-q = deque()
-q.append(N)
+    INF = 10e9
+    visited = [INF for _ in range(LIMIT + 1)]
 
-visitied = [True for _ in range(100001)]
+    # i = 0
+    # while N * (2 ** i) <= LIMIT:
+    #     visited[N * (2 ** i)] = 0
+    #     i += 1
+    visited[N] = 0
+    while q:
+        x = q.popleft()
 
-visitied[N] = 0
+        if 0 <= 2 * x <= LIMIT and (visited[x] < visited[2 * x]):
+            q.append(2 * x)
+            visited[2 * x] = visited[x]
 
-while q:
-    x = q.popleft()
-    if x == K:
-        break
+        if 0 <= x + 1 <= LIMIT and (visited[x] + 1 < visited[x + 1]):
+            q.append(x + 1)
+            visited[x + 1] = visited[x] + 1
+
+        if 0 <= x - 1 <= LIMIT and (visited[x] + 1 < visited[x - 1]):
+            q.append(x - 1)
+            visited[x - 1] = visited[x] + 1
+
+    print(visited[K])
